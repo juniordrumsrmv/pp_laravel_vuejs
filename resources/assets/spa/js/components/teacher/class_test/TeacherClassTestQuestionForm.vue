13e9aae0e6a91d9fcb37778240d79c4aaab90974
Bianou Oliveira <junordrumsrmv@gmail.com>
2019-03-05 10:52:05 -0300

    =AAA

---
+++ resources/assets/spa/js/components/teacher/class_test/TeacherClassTestQuestionForm.vue
@@ -0,0 +1,25 @@
+<template>
+    <form class="form-horizontal" @submit.prevent="addQuestion">
+        <div class="form-group">
+            <label for="question" class="control-label">Questão</label>
+            <texteare id="question" name="question" class="form-control"></texteare>
+        </div>
+        <div class="form-group">
+            <label for="point" class="control-label">Pontos</label>
+            <texteare id="point" name="point" class="form-control"></texteare>
+        </div>
+    </form>
+</template>
+
+<script type="text/javascript">
+    import store from '../../../store/store';
+
+    export default {
+        computed: {
+
+        },
+        methods: {
+
+        }
+    }
+</script>
